ed907230df49faf78af611f257d5f8cb631bee96
michaellee8 <ckmichael8@gmail.com>
2016-10-22 00:57:32 +0800

    developing sold() function

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
 // var prev = ["","",""];
 
 var CurrentMode = 0;
+var db = new alasql.Database();
 // 1 -> edit, 2 -> report, 3 -> sell, 4 -> borrow, 5 -> return
 
-function save_erase(id, mode) {
-    // prev[CurrentMode] = documenet.getElementById(id).innerHTML;
-    CurrentMode = mode;
-    document.getElementById(id).innerHTML = "";
-    //erase the previous content of the target div
-}
-
-function sold(ItemId) {
-
-}
-
-function switch_sell(id) {
-
-    save_erase(id, 3);
-    var db = new alasql.Database();
+function _init_() {
 
     // Below code is for debug test, will be corrected later when edit function is developed
     db.exec("CREATE TABLE Sell_Items (name STRING, number INT, price FLOAT)");
@@ -63,34 +50,31 @@ function switch_sell(id) {
         price : 100
     }];
     // Above code is for debug test, will be corrected later when edit function is developed
+}
 
-    // generate the table for sell item selection with innerHTML
-    /* Rendering table with innerHTML is deprecated since it is unstable, slowers the speed and could break the whole system down
-    document.getElementById(id).innerHTML += '<table width="49%">';
-    for (i in db.tables.Sell_Items.data){
-    if (i%3==0){
-    document.getElementById(id).innerHTML += "<tr>";
-    }
-    document.getElementById(id).innerHTML += '<td><input ' + db.tables.Sell_Items.data[i].number==0?'disabled="disabled"':'' + ' onclick="sold('+i.toString()+')" '+'type="button" id="'+ db.tables.Sell_Items.data[i].name + '_sold_btn" ' + 'value="' + db.tables.Sell_Items.data[i].name + ' $' + db.tables.Sell_Items.data[i].price.toString() +'"></td>';
-    if (i % 3 == 2 || i == db.tables.Sell_Items.data.length) {
+function save_erase(id, mode) {
+    // prev[CurrentMode] = documenet.getElementById(id).innerHTML;
+    CurrentMode = mode;
+    document.getElementById(id).innerHTML = "";
+    //erase the previous content of the target div
+}
 
-    document.getElementById(id).innerHTML += "</tr>";
-    }
-    }
-    document.getElementById(id).innerHTML += "</table>";
+function sold(name, number) {
 
-    //generate the table for borrow item selection
-    document.getElementById(id).innerHTML += '<table width="49%>';
+}
 
-    document.getElementById(id).innerHTML += "</table>";
+function getListText(name, price, number) {
+    return name + '  Price: $' + price + '  Number left: ' + number;
+}
 
-    document.getElementById(id).innerHTML += "<br/>";
-    */
+function switch_sell(id) {
 
-    // generate the drop down list for item selection with HTML DOM append child method
+    save_erase(id, 3);
+
     var form = $('<form></form>', {
         id : "sell_item_selection_form"
     });
+    $(form).append('What do you want to buy? ');
     var sell_select_item = $('<select></select>', {
         id : "sell_item_selector"
     });
@@ -103,10 +87,18 @@ function switch_sell(id) {
         $(sell_select_item).append($('<option></option>', {
             id : 'sell_item_selector_option_' + db.tables.Sell_Items.data[i].name,
             value : db.tables.Sell_Items.data[i].name,
-            text : db.tables.Sell_Items.data[i].name
+            text : getListText(db.tables.Sell_Items.data[i].name, db.tables.Sell_Items.data[i].price, db.tables.Sell_Items.data[i].number)
         }));
     }
     $(form).append(sell_select_item);
+    $(form).append('<br/><br/>\nHow many do you want? ');
+    $(form).append($('<input id="sell_number" type="number"/>'));
+    $(form).append('<br/><br/>');
+    $(form).append($('<button></button>', {
+        id : "sell_confirm",
+        text : "confirm",
+        type : "button",
+        onclick : 'sold(sell_select_item.value,sell_number.value)'
+    }));
     $('#' + id).append(form);
-
 }
